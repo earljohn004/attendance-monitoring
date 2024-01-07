daa1630f23e859fe99155aa6761ba2c7dbb85c7d
earljohn004 <earl.abaquita@gmail.com>
2024-01-07 16:35:14 +0800

    add arduino serial code

---
+++ arduino_receiver.py
@@ -0,0 +1,33 @@
+import serial
+import time
+
+
+class ArduinoReceiver:
+    def __init__(self, port="COM1", baud_rate=9600):
+        self.arduino_port = port
+        self.baud_rate = baud_rate
+        self.timeout = 10
+        self.delay_start = 5
+        self.serial = serial.Serial(
+            self.arduino_port, self.baud_rate, timeout=self.timeout
+        )
+
+    def read_serial(self):
+        time.sleep(self.delay_start)
+        try:
+            while True:
+                if self.serial.in_waiting > 0:
+                    data = self.serial.readline().decode("utf-8").strip()
+                    print(f"Received from Arduino: {data}")
+
+        except serial.SerialException as e:
+            print(f"Error: {e}")
+
+        finally:
+            if self.serial.is_open:
+                self.serial.close()
+
+
+if __name__ == "__main__":
+    arduino = ArduinoReceiver()
+    arduino.read_serial()
